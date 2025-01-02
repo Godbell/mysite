41075c9e2d40f30d8c3968928f3a3116ae605713
Godbell <12191579@inha.edu>
2025-01-02 14:30:39 +0900

    Feat: add exception logger

--- mysite03/src/main/java/mysite/exception/GlobalExceptionHandler.java
+++ mysite03/src/main/java/mysite/exception/GlobalExceptionHandler.java
@@ -3,17 +3,23 @@ package mysite.exception;
 import java.io.PrintWriter;
 import java.io.StringWriter;
 
+import org.apache.commons.logging.Log;
+import org.apache.commons.logging.LogFactory;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.ControllerAdvice;
 import org.springframework.web.bind.annotation.ExceptionHandler;
 
 @ControllerAdvice
 public class GlobalExceptionHandler {
+    private static final Log log = LogFactory.getLog(GlobalExceptionHandler.class);
+
     @ExceptionHandler(Exception.class)
-    public String handler(Exception e) {
+    public String handler(Model model, Exception e) {
         StringWriter stringWriter = new StringWriter();
         e.printStackTrace(new PrintWriter(stringWriter));
-        System.out.println(stringWriter.toString());
+        log.error(stringWriter);
 
+        model.addAttribute("errors", stringWriter.toString());
         return "errors/exception";
     }
 }
